bd8096adb2e82d2b22f2fc973055edc11cb3ca0c
David Bowland <dbowland1@gmail.com>
2017-05-25 22:45:25 -0500

    Add defer information to demonstration page

--- index.php
+++ index.php
@@ -11,8 +11,8 @@ function getTimeURI($path) {
   <meta http-equiv="content-type" content="text/html;charset=utf-8">
   <meta name="viewport" content="width=device-width,initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="IE=Edge">
-  <!-- Fancy import of smooth-scroll.js -->
-  <script src="<?php echo getTimeURI('smooth-scroll.js'); ?>"></script>
+  <!-- Fancy import of smooth-scroll.js, defer is encouraged -->
+  <script src="<?php echo getTimeURI('smooth-scroll.js'); ?>" defer></script>
   <!-- Styles for demonstration only -->
   <style type="text/css">
 .scrollRow {

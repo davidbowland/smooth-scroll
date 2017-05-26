399d05213500ae2733b84abece1d7712e56fda3d
David Bowland <dbowland1@gmail.com>
2017-05-25 22:54:37 -0500

    Update demonstration page to exhibit more functionality

--- index.php
+++ index.php
@@ -36,12 +36,14 @@ function getTimeURI($path) {
   <div class="scrollRow">
     <div id="cell1">
       1<br />
+      <!-- Adding data-smooth-scroll-to="#" makes this link scroll smoothly when JavaScript is enabled -->
       <a href="#cell4" data-smooth-scroll-to="#">Down 1</a><br />
       <a href="#cell7" data-smooth-scroll-to="#">Down 2</a>
     </div>
     <div id="cell2">
       2<br />
-      <a href="#cell5" data-smooth-scroll-to="#">Down 1</a><br />
+      <!-- data-smooth-scroll-duration can be used to specify scroll duration, in milliseconds -->
+      <a href="#cell5" data-smooth-scroll-to="#" data-smooth-scroll-duration="3000">Down 1, 3 sec</a><br />
       <a href="#cell7" data-smooth-scroll-to="#">Left 1, Down 2</a><br />
     </div>
     <div id="cell3">
@@ -50,27 +52,30 @@ function getTimeURI($path) {
       <a href="#cell5" data-smooth-scroll-to="#">Left 1, Down 1</a><br />
     </div>
   </div>
-  <div class="scrollRow">
+  <!-- Test: data-smooth-scroll-hash and data-smooth-scroll-duration -->
+  <!-- Adding data-smooth-scroll-hash="true" makes all # links in this container scroll smoothly -->
+  <div class="scrollRow" data-smooth-scroll-hash="true">
     <div id="cell4">
       4<br />
-      <button data-smooth-scroll-to="cell6" data-smooth-scroll-duration="3000">Right 2, 3 sec</button><br />
-      <button data-smooth-scroll-to="cell7">Down 1</button>
+      <a href="#cell6" data-smooth-scroll-duration="3000">Right 2, 3 sec</a><br />
+      <a href="#cell7">Down 1</a>
     </div>
     <div id="cell5">
       5<br />
-      <button data-smooth-scroll-to="cell2" data-smooth-scroll-duration="3000">Up 1, 3 sec</button><br />
-      <button data-smooth-scroll-to="cell9">Right 1, Down 1</button>
+      <a href="#cell2" data-smooth-scroll-duration="3000">Up 1, 3 sec</a><br />
+      <a href="#cell9">Right 1, Down 1</a>
     </div>
     <div id="cell6">
       6<br />
-      <button data-smooth-scroll-to="cell3" data-smooth-scroll-duration="3000">Up 1, 3 sec</button><br />
-      <button data-smooth-scroll-to="cell8">Left 1, Down 1</button>
+      <a href="#cell3" data-smooth-scroll-duration="3000">Up 1, 3 sec</a><br />
+      <a href="#cell8">Left 1, Down 1</a>
     </div>
   </div>
   <!-- Test: data-smooth-scroll-to with ID value and data-smooth-scroll-duration -->
   <div class="scrollRow">
     <div name="cell7">
       7<br />
+      <!-- Specify a scroll target by ID using data-smooth-scroll-to -->
       <button data-smooth-scroll-to="cell3" data-smooth-scroll-duration="5000">Up 2, Right 2, 5 sec</button><br />
       <button data-smooth-scroll-to="cell9">Right 2</button>
     </div>
